698a0e6d1f5c245efa0b2f825cd5e5bbb0d049a2
Muhd-Yusuf <ymohd0627@gmail.com>
2026-05-02 22:49:53 +0100

    Remove X-Powered-By and Server headers

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  poweredByHeader: false,
+  headers: async () => [
+    {
+      source: "/(.*)",
+      headers: [
+        { key: "X-Powered-By", value: "" },
+        { key: "Server", value: "" },
+      ],
+    },
+  ],
 };
 
 export default nextConfig;
